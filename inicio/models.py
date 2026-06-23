a01d0c299754b6cf49b4079b7fcfe9dbca367499
camilatuso9119-coder <camilatuso9119@utc.edu.ec>
2026-06-22 22:44:16 -0500

    Actualizar campos del modelo para veterinaria

--- inicio/models.py
+++ inicio/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class CitaVeterinaria(models.Model):
-    name = models.CharField(max_length=120, verbose_name='Nombre')
-    phone = models.CharField(max_length=30, verbose_name='Teléfono')
+    nombre_mascota = models.CharField(max_length=120, verbose_name='Nombre de la mascota')
+    nombre_dueno = models.CharField(max_length=120, verbose_name='Nombre del dueño')
+    telefono = models.CharField(max_length=30, verbose_name='Teléfono')
     email = models.EmailField(blank=True, verbose_name='Correo')
-    address = models.CharField(max_length=255, blank=True, verbose_name='Dirección')
-    people = models.PositiveSmallIntegerField(default=1, verbose_name='Personas')
-    date = models.DateField(null=True, blank=True, verbose_name='Fecha')
+    direccion = models.CharField(max_length=255, blank=True, verbose_name='Dirección')
+    tipo_mascota = models.CharField(max_length=50, verbose_name='Tipo de mascota')
+    fecha = models.DateField(null=True, blank=True, verbose_name='Fecha de cita')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -16,4 +17,4 @@ class CitaVeterinaria(models.Model):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'{self.name} - {self.people} persona(s)'
+        return f'{self.nombre_mascota} - {self.nombre_dueno}'
